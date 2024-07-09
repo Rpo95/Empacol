9abaed868aa56f1cb58bc970e59a5104df5d879d
Mateo <teo9515@gmail.com>
2024-07-08 20:37:42 -0500

    empacol21

--- components/Constants.ts
+++ components/Constants.ts
@@ -3,25 +3,25 @@
 export const images = [
     {
       id: 1,
-      src: "/contenedor.jpg",
+      src: "/contenedor.webp",
       title: "Container Blocking & Bracing",
       desc: "Utilizing custom designed blocking and bracing our experienced staff can prevent moving or shifting of oversize machinery, vehicles, copiers, or other equipment inside a container during handling and transport",
     },
     {
       id: 2,
-      src: "/vapor.jpg",
+      src: "/vapor.webp",
       title: "Water Vapor Barrier Protection",
       desc: "Protection will protect your equipment or machinery from corrosion or moisture damage; this is accomplished by fully enclosing the item being packaged in a foil barrier material. Inside the package a clay or silica gel is added in sufficient amounts to absorb moisture within the bag and maintaining a twenty to thirty percent relative humidity. Forty percent or more is the start point of corrosion.",
     },
     {
       id: 3,
-      src: "/shrink.jpg",
+      src: "/shrink.webp",
       title: "Shrink Wrap Packing",
       desc: "When export boxing is not necessary and  surface protection against scratches, dirt and grime is the major concerns, Shrink Packing is an option that provides a form-fitting barrier that resist wind, rain, dust, dirt and other elements that may affect your Cargo, Boats, Engines, Aircraft Parts or any large size objects.",
     },
     {
       id: 4,
-      src: "/foam.jpg",
+      src: "/foam.webp",
       title: "Foam in Place Packing",
       desc: "This is the ultimate way to package delicate items such as glass, aircraft arts, electronics, furniture, antiques and much more. This high quality polyurethane foam cushion protects your products from shock & vibration  during shipping and handling.",
     },
@@ -31,25 +31,25 @@ export const images = [
   export const crates = [
     {
       id: 1,
-      src: "/crate.jpg",
+      src: "/crate.webp",
       title: "Reusable Crates",
       desc: "Empacol designs and builds quality reusable Crates which provide a secure, traveling Case for your product while being transported throughout the World. These crates are built to withstand the abuses subjected to during transportation.",
     },
     {
       id: 2,
-      src: "/packing.jpg",
+      src: "/packing.webp",
       title: "On Site Packaging",
       desc: "Empacol Packing and Crating provides local On Site Packaging.  Our On Site packaging crews are available for any project.  Each crew is equipped with all the necessary materials and tools to complete the packaging job on location. Crating construction can be completed prior to the job date and assembled on location at your convenience.",
     },
     {
       id: 3,
-      src: "/shock.jpg",
+      src: "/shock.webp",
       title: "Shock & Vibration Protection",
       desc: "Shock is defined as single pulse event such as dropping a package onto a  concrete floor.  Vibration is defined as a continuous sine-wave motion subjecting a product to varying  G-Loads The best protection for sensitive equipment during regular or rough handling is made  with soft materials such as Styrofoam, Poly-Foams and expanded Polyester Urethane.",
     },
     {
       id: 4,
-      src: "/export.jpg",
+      src: "/export.webp",
       title: "Export Crating",
       desc: "Empacol manufactures a wide variety of custom crates and skids for air freight, container freight and breakbulk shipments. Whether you need boxing for a small shipment or a large one, Empacol will handle your material with great care. We use a wide range of packaging materials to protect your product from the hazards of overseas shipping.",
     },
