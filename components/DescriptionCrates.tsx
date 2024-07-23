d8cadf268bd2aa0ea466f157a7f109d721b08029
Mateo <teo9515@gmail.com>
2024-07-23 14:41:27 -0500

    fix23

--- components/DescriptionCrates.tsx
+++ components/DescriptionCrates.tsx
@@ -6,7 +6,7 @@ export default function DescriptionCrates({
 }: DescriptionCratesProps) {
   return (
     <div
-      className={`max-h-[286px] md:max-h-[247px] lg:max-h-[315px] py-3 w-full mb-10 md:mb-0  ${
+      className={`max-h-[286px] md:max-h-[315px] py-3 w-full mb-10 md:mb-0  ${
         activeImage % 2 === 0 ? "bg-[#ff9500]" : "bg-[#E96701]"
       }  relative h-full flex justify-center md:rounded-tr-lg md:rounded-br-lg md:rounded-b-none rounded-b-lg`}
     >
